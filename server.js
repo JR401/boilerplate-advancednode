3dbdf0db9ce81218c5ddc2d18e18c0dc8030fafb
JR401 <joramiranda8@gmail.com>
2024-12-04 09:44:46 -0600

    Update server.js

--- server.js
+++ server.js
@@ -36,9 +36,7 @@ passport.serializeUser((user, done) => {
 });
 
 passport.deserializeUser((id, done) => {
-  // myDataBase.findOne({ _id: new ObjectID(id) }, (err, doc) => {
     done(null, null);
-  // });
 });
 
 const PORT = process.env.PORT || 3000;
